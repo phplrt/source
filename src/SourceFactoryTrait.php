0d73ae48162f2d9784aafa226a2828c884d002de
SerafimArts <nesk@xakep.ru>
2026-01-16 05:53:54 +0000

    Use list<T> in ArrayBuffer instead of custom keys (possibly keys duplication fix).

--- src/SourceFactoryTrait.php
+++ src/SourceFactoryTrait.php
@@ -8,6 +8,7 @@ use Phplrt\Contracts\Source\FileInterface;
 use Phplrt\Contracts\Source\ReadableInterface;
 use Phplrt\Contracts\Source\SourceExceptionInterface;
 use Phplrt\Contracts\Source\SourceFactoryInterface;
+use Psr\Http\Message\StreamInterface;
 
 trait SourceFactoryTrait
 {
@@ -31,11 +32,13 @@ trait SourceFactoryTrait
      *         : ReadableInterface)
      * )
      * @throws SourceExceptionInterface
-     *
-     * @psalm-suppress NoValue : Allow any value
      */
     public static function new(mixed $source): ReadableInterface
     {
+        if ($source instanceof StreamInterface) {
+            return static::fromPsrStream($source);
+        }
+
         $factory = self::getSourceFactory();
 
         return $factory->create($source);
@@ -46,6 +49,7 @@ trait SourceFactoryTrait
      * @param non-empty-string|null $pathname
      *
      * @return ($pathname is null ? ReadableInterface : FileInterface)
+     * @throws SourceExceptionInterface
      */
     public static function empty(?string $pathname = null): ReadableInterface
     {
@@ -57,6 +61,7 @@ trait SourceFactoryTrait
      * @param non-empty-string|null $pathname
      *
      * @return ($pathname is null ? ReadableInterface : FileInterface)
+     * @throws SourceExceptionInterface
      */
     public static function fromSources(string $sources, ?string $pathname = null): ReadableInterface
     {
@@ -95,6 +100,24 @@ trait SourceFactoryTrait
         }
 
         return new File($pathname);
+    }
+
+    /**
+     * @param non-empty-string|null $pathname
+     *
+     * @return ($pathname is null ? ReadableInterface : FileInterface)
+     * @throws SourceExceptionInterface
+     *
+     * @deprecated since phplrt 3.4 and will be removed in 4.0, use {@see fromResource()} instead.
+     */
+    public static function fromPsrStream(StreamInterface $stream, ?string $pathname = null): ReadableInterface
+    {
+        trigger_deprecation('phplrt/source', '3.4', <<<'MSG'
+            Using "%s::fromPsrStream($stream)" with %s argument is deprecated,
+            use "%1$s::fromResource($stream->detach())" instead.
+            MSG, static::class, $stream::class);
+
+        return static::fromResource($stream->detach(), $pathname);
     }
 
     /**
--- src/Stream.php
+++ src/Stream.php
@@ -15,61 +15,39 @@ class Stream extends Readable
      * reading the data.
      *
      * @var int<0, max>
-     *
-     * @psalm-readonly-allow-private-mutation
      */
-    private int $offset;
+    private readonly int $offset;
 
-    /**
-     * @var resource
-     *
-     * @psalm-readonly-allow-private-mutation
-     */
-    private $stream;
-
-    /**
-     * @var non-empty-string
-     *
-     * @psalm-readonly-allow-private-mutation
-     */
-    private string $algo = SourceFactory::DEFAULT_HASH_ALGO;
-
-    /**
-     * @var int<1, max>
-     *
-     * @psalm-readonly-allow-private-mutation
-     */
-    private int $chunkSize = SourceFactory::DEFAULT_CHUNK_SIZE;
-
-    /**
-     * @param resource $stream
-     * @param non-empty-string $algo hashing algorithm for the source
-     * @param int<1, max> $chunkSize the chunk size used while non-blocking
-     *        reading the file inside the {@see \Fiber}
-     */
     public function __construct(
-        $stream,
-        string $algo = SourceFactory::DEFAULT_HASH_ALGO,
-        int $chunkSize = SourceFactory::DEFAULT_CHUNK_SIZE
+        /**
+         * @var resource
+         */
+        private readonly mixed $stream,
+        /**
+         * Hashing algorithm for the source.
+         *
+         * @var non-empty-string
+         */
+        private readonly string $algo = SourceFactory::DEFAULT_HASH_ALGO,
+        /**
+         * The chunk size used while non-blocking reading the file inside
+         * the {@see \Fiber}.
+         *
+         * @var int<1, max>
+         */
+        private readonly int $chunkSize = SourceFactory::DEFAULT_CHUNK_SIZE
     ) {
         assert(\is_resource($stream), 'Stream argument must be a valid resource stream');
         assert($algo !== '', 'Hashing algorithm name must not be empty');
         assert($chunkSize >= 1, 'Chunk size must be greater than 0');
 
-        /** @psalm-suppress PropertyTypeCoercion */
         $this->offset = (int) \ftell($stream);
-        $this->chunkSize = $chunkSize;
-        $this->algo = $algo;
-        $this->stream = $stream;
     }
 
     public function getContents(): string
     {
         try {
-            if (\PHP_MAJOR_VERSION >= 8
-                && \PHP_MINOR_VERSION >= 1
-                && \Fiber::getCurrent() !== null
-            ) {
+            if (\Fiber::getCurrent() !== null) {
                 return $this->asyncGetContents();
             }
 
@@ -118,7 +96,7 @@ class Stream extends Readable
         return $result;
     }
 
-    public function getStream()
+    public function getStream(): mixed
     {
         return $this->stream;
     }
@@ -153,7 +131,7 @@ class Stream extends Readable
 
         $meta = @\stream_get_meta_data($this->stream);
 
-        if (\error_get_last()) {
+        if (\error_get_last() !== null) {
             return [];
         }
 
